15aa1c01d354e38f71326c356f61c9b5258a85a7
shunhamm <shun.022885@gmail.com>
2023-10-18 11:56:34 +0900

    アソシエーションの追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
 class CommentsController < ApplicationController
   def new
+    @comment = Comment.new
   end
 
   def index
+    @comments = Comment.all
   end
 
-  def create
-  end
+  def create; end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
 class Comment < ApplicationRecord
+  belongs_to :user, :report
 end
