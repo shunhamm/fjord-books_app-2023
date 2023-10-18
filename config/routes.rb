992de860eee444039acb5e02b59836a00c3b9c9c
shunhamm <shun.022885@gmail.com>
2023-10-18 11:40:44 +0900

    generate Commnets(model, controller, view)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'comments/new'
+  get 'comments/index'
+  get 'comments/create'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
   resources :books
   resources :reports
+  resources :comments
   resources :users, only: %i(index show)
 end
