32abfd5e57c603ed724e5be3455e1d3f1376a5ed
shunhamm <shun.022885@gmail.com>
2023-10-22 14:14:34 +0900

    reportsでもコメント機能が扱えるようにした

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -7,7 +7,9 @@ class ReportsController < ApplicationController
   end
 
   # GET /reports/1 or /reports/1.json
-  def show; end
+  def show
+    @comments = @report.comments
+  end
 
   # GET /reports/new
   def new
