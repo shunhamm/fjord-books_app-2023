32988c90a643bd41bd9872efd000fcb95159680b
shunhamm <shun.022885@gmail.com>
2023-10-22 14:03:58 +0900

    コメント一覧に表示する内容を変更。その他フォーマットに従った修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,8 +13,8 @@ class CommentsController < ApplicationController
   def create
     @comment = @commentable.comments.build(comment_params)
     @comment.user_id = current_user.id
-    @comment.save!
-    redirect_to [@commentable, @comment]
+    @comment.save
+    redirect_to [@commentable]
   end
 
   private
