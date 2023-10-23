fee0f45e71b597677ec31e41d803d99966a91114
shunhamm <shun.022885@gmail.com>
2023-10-23 11:31:50 +0900

    ユーザ削除時等に関連削除を行う様に

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,5 +3,5 @@
 class Book < ApplicationRecord
   mount_uploader :picture, PictureUploader
 
-  has_many :comments, as: :commentable
+  has_many :comments, as: :commentable, dependent: :destroy
 end
