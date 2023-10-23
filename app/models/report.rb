131a1b511ff91275b2a5ae1f9f46491efb84cd55
shunhamm <shun.022885@gmail.com>
2023-10-23 10:50:21 +0900

    ユーザー関連をレポートに追加

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,4 +2,5 @@
 
 class Report < ApplicationRecord
   has_many :comments, as: :commentable
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :comments
+  has_many :reports
 
   has_one_attached :avatar do |attachable|
     attachable.variant :thumb, resize_to_limit: [150, 150]
