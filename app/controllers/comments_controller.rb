bb9433d697329dc44dc0f365abce6091304c15e9
shunhamm <shun.022885@gmail.com>
2023-10-23 10:47:04 +0900

    コメント削除機能を追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
+  before_action :set_comment, only: %i[destroy]
   before_action :set_commentable
   def new
     @comment = Comment.new
@@ -17,12 +18,25 @@ class CommentsController < ApplicationController
     redirect_to [@commentable]
   end
 
+  def destroy
+    @comment.destroy
+
+    respond_to do |format|
+      format.html { redirect_to [@commentable], notice: t('controllers.common.notice_destroy', name: Comment.model_name.human) }
+      format.json { head :no_content }
+    end
+  end
+
   private
 
   # https://nekorails.hatenablog.com/entry/2019/06/13/031003　より
   def set_commentable
     resource, id = request.path.split('/')[1, 2]
     @commentable = resource.singularize.classify.constantize.find(id)
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
   end
 
   def comment_params
