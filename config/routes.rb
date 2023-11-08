a3da28ea1461bcb83b09dfe01cfae1b8ffcc217d
shunhamm <shun.022885@gmail.com>
2023-11-08 14:32:27 +0900

    configの変更。htmlタグの適切化

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
-  resources :books
-  resources :reports
   resources :books do
-    resources :comments, module: :books
+    resources :comments, module: :books, only: %i(create destroy)
   end
   resources :reports do
     resources :comments, module: :reports, only: %i(create destroy)
