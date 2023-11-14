1234fd960d24b00e7424a7e58b8abaddb7ab84b1
shunhamm <shun.022885@gmail.com>
2023-11-14 11:32:40 +0900

    言及関係にある日報の一覧を取得するメソッドをReportに分離。i18n国際化に対応

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,23 +4,29 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
-  has_many :report_links, dependent: :destroy
+  has_many :report_links, foreign_key: 'from_report_id', dependent: :destroy, inverse_of: :from_report
   has_many :to_reports, through: :report_links, source: :to_report
-  has_many :my_report_links, class_name: 'ReportLink', foreign_key: 'to_report_id', dependent: :destroy
-  has_many :reports, through: :my_report_links, source: :report
+  has_many :my_report_links, class_name: 'ReportLink', foreign_key: 'to_report_id', dependent: :destroy, inverse_of: :to_report
+  has_many :from_reports, through: :my_report_links, source: :report
 
   validates :title, presence: true
   validates :content, presence: true
 
+  def create_report_link
+    report_links.destroy_all
+    mentioning_report_ids = content.scan(%r{http://127.0.0.1:3000/reports/(\d+)})
+    return if mentioning_report_ids.empty?
+
+    mentioning_report_ids.each do |id|
+      to_report = Report.find(id[0].to_i)
+      link(to_report)
+    end
+  end
+
   def link(to_report)
     return if self == to_report
 
-    report_links.find_or_create_by(to_report_id: to_report.id)
-  end
-
-  def unlink(to_report)
-    report_link_to = report_links.find_by(to_report_id: to_report.id)
-    report_link_to&.destroy
+    report_links.find_or_create_by!(to_report_id: to_report.id)
   end
 
   def editable?(target_user)
@@ -29,5 +35,13 @@ class Report < ApplicationRecord
 
   def created_on
     created_at.to_date
+  end
+
+  def mentioning_reports
+    report_links.order(:id).presence
+  end
+
+  def mentioned_reports
+    my_report_links.order(:id).presence
   end
 end
