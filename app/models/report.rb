c09b495b7c119a52f5484ca631ac3e62624ac1bf
shunhamm <shun.022885@gmail.com>
2023-11-15 12:13:25 +0900

    言及を表す関連名を変更したがタイポがあったので関連を修正

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,7 +4,7 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
-  has_many :mentioned_links, foreign_key: 'mentioned_report_id', dependent: :destroy, inverse_of: :mentioned_report
+  has_many :mentioned_by_reports, foreign_key: 'mentioned_report_id', dependent: :destroy, inverse_of: :mentioned_report
   has_many :mentioning_reports, through: :mentioned_by_reports, source: :mentioning_report
   has_many :mentions_to_reports, class_name: 'ReportLink', foreign_key: 'mentioning_report_id', dependent: :destroy, inverse_of: :mentioning_report
   has_many :mentioned_reports, through: :mentions_to_reports, source: :report
@@ -13,7 +13,7 @@ class Report < ApplicationRecord
   validates :content, presence: true
 
   def create_report_link
-    mentioned_links.destroy_all
+    mentioned_by_reports.destroy_all
     mentioning_report_ids = content.scan(%r{http://127.0.0.1:3000/reports/(\d+)})
 
     mentioning_report_ids.each do |id|
@@ -31,11 +31,11 @@ class Report < ApplicationRecord
   end
 
   def mentioning_reports
-    mentioned_links.order(:id).presence
+    mentioned_by_reports.order(:id).presence
   end
 
   def mentioned_reports
-    my_mentioned_links.order(:id).presence
+    my_mentioned_by_reports.order(:id).presence
   end
 end
 
@@ -44,5 +44,5 @@ private
 def link(mentioning_report)
   return if self == mentioning_report
 
-  mentioned_links.find_or_create_by!(mentioning_report_id: mentioning_report.id)
+  mentioned_by_reports.find_or_create_by!(mentioning_report_id: mentioning_report.id)
 end
