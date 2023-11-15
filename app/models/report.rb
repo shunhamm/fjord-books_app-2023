7fe1049411f3f5ec3e97b62ce19a5b18c92ec52a
shunhamm <shun.022885@gmail.com>
2023-11-15 14:45:50 +0900

    関連の変更忘れを修正

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,10 +4,10 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
-  has_many :mentioned_by_reports, foreign_key: 'mentioned_report_id', dependent: :destroy, inverse_of: :mentioned_report
+  has_many :mentioned_by_reports, class_name: 'ReportLink', foreign_key: 'mentioned_report_id', dependent: :destroy, inverse_of: :mentioned_report
   has_many :mentioning_reports, through: :mentioned_by_reports, source: :mentioning_report
   has_many :mentions_to_reports, class_name: 'ReportLink', foreign_key: 'mentioning_report_id', dependent: :destroy, inverse_of: :mentioning_report
-  has_many :mentioned_reports, through: :mentions_to_reports, source: :report
+  has_many :mentioned_reports, through: :mentions_to_reports, source: :mentioned_report
 
   validates :title, presence: true
   validates :content, presence: true
--- app/models/report_link.rb
+++ app/models/report_link.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class ReportLink < ApplicationRecord
-  belongs_to :mentioned_reports, class_name: 'Report', inverse_of: :report_links
-  belongs_to :mentioning_reports, class_name: 'Report', inverse_of: :my_report_links
+  belongs_to :mentioned_report, class_name: 'Report', inverse_of: :mentioned_by_reports
+  belongs_to :mentioning_report, class_name: 'Report', inverse_of: :mentions_to_reports
 
-  validates :mentioned_reports, uniqueness: { scope: :mentioning_reports }
+  validates :mentioned_report, uniqueness: { scope: :mentioning_report }
 end
