0c211de1852a98e850ba9877ee68fa3cf1c61e3f
shunhamm <shun.022885@gmail.com>
2023-11-16 12:05:05 +0900

    言及関係の命名を統一した名詞句に変更。関連ファイルをそれに合わせた。

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,16 +4,16 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
-  has_many :mentioned_by_reports, class_name: 'ReportLink', foreign_key: 'mentioned_report_id', dependent: :destroy, inverse_of: :mentioned_report
-  has_many :mentioning_reports, through: :mentioned_by_reports, source: :mentioning_report
-  has_many :mentions_to_reports, class_name: 'ReportLink', foreign_key: 'mentioning_report_id', dependent: :destroy, inverse_of: :mentioning_report
-  has_many :mentioned_reports, through: :mentions_to_reports, source: :mentioned_report
+  has_many :outgoing_mentions, class_name: 'ReportLink', foreign_key: 'mentioning_report_id', dependent: :destroy, inverse_of: :mentioned_report
+  has_many :mentioning_reports, through: :outgoing_mentions, source: :mentioned_report
+  has_many :incoming_mentions, class_name: 'ReportLink', foreign_key: 'mentioned_report_id', dependent: :destroy, inverse_of: :mentioning_report
+  has_many :mentioned_reports, through: :incoming_mentions, source: :mentioning_report
 
   validates :title, presence: true
   validates :content, presence: true
 
   def create_report_link
-    mentioned_by_reports.destroy_all
+    outgoing_mentions.destroy_all
     mentioning_report_ids = content.scan(%r{http://127.0.0.1:3000/reports/(\d+)})
 
     mentioning_report_ids.each do |id|
@@ -33,8 +33,8 @@ class Report < ApplicationRecord
   private
 
   def link(mentioning_report)
-    return if self == mentioning_report
+    return if id == mentioning_report.id
 
-    mentioned_by_reports.find_or_create_by!(mentioning_report_id: mentioning_report.id)
+    mentioning_report.outgoing_mentions.find_or_create_by!(mentioned_report: self)
   end
 end
--- app/models/report_link.rb
+++ app/models/report_link.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class ReportLink < ApplicationRecord
-  belongs_to :mentioned_report, class_name: 'Report', inverse_of: :mentioned_by_reports
-  belongs_to :mentioning_report, class_name: 'Report', inverse_of: :mentions_to_reports
+  belongs_to :mentioned_report, class_name: 'Report', inverse_of: :incoming_mentions
+  belongs_to :mentioning_report, class_name: 'Report', inverse_of: :outgoing_mentions
 
   validates :mentioned_report, uniqueness: { scope: :mentioning_report }
 end
